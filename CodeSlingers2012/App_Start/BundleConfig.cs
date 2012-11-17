4c7b829979677ac6cf6c586284164314bc165198
Nathan Hase <hase.nate@gmail.com>
2012-11-17 01:53:55 -0600

    Break nav and home content into Display Templates.

--- CodeSlingers2012/App_Start/BundleConfig.cs
+++ CodeSlingers2012/App_Start/BundleConfig.cs
@@ -26,11 +26,9 @@ namespace CodeSlingers2012
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/css").Include(
-                        "~/css/style.css"));
+            bundles.Add(new StyleBundle("~/css").Include("~/css/style.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
