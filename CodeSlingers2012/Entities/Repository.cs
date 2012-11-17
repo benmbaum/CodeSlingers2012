de00e69e150ef64826472b05a041fa11330801a8
Ben Baum <benmbaum@gmail.com>
2012-11-17 03:49:28 -0600

    Added save call to repository for admin purposes.

--- CodeSlingers2012/Entities/Repository.cs
+++ CodeSlingers2012/Entities/Repository.cs
@@ -13,7 +13,7 @@ namespace CodeSlingers2012.Entities
 
             using (var repo = new CodeSlingers2012Entities())
             {
-                items = (from r in repo.SectionContents 
+                items = (from r in repo.SectionContents
                          orderby r.Section, r.SortOrder
                          select r).ToList();
             }
@@ -26,8 +26,8 @@ namespace CodeSlingers2012.Entities
 
             using (var repo = new CodeSlingers2012Entities())
             {
-                items = (from r in repo.SectionContents 
-                         where r.Section == (int)section 
+                items = (from r in repo.SectionContents
+                         where r.Section == (int)section
                          orderby r.SortOrder
                          select r).ToList();
             }
@@ -40,11 +40,47 @@ namespace CodeSlingers2012.Entities
 
             using (var repo = new CodeSlingers2012Entities())
             {
-                item = (from r in repo.SectionContents 
-                        where r.Section == (int)section && r.SectionId == sectionId 
+                item = (from r in repo.SectionContents
+                        where r.Section == (int)section && r.SectionId == sectionId
                         select r).FirstOrDefault();
             }
             return item;
+        }
+
+        public void SaveSectionContent(SectionContent sectionItem)
+        {
+            SectionContent item;
+            var insert = false;
+
+            using (var repo = new CodeSlingers2012Entities())
+            {
+                item = (from r in repo.SectionContents
+                        where r.Id == sectionItem.Id
+                        select r).FirstOrDefault();
+
+                if (item == null)
+                {
+                    item = new SectionContent();
+                    insert = true;
+                }
+
+                item.ImageUrl = sectionItem.ImageUrl;
+                item.Section = sectionItem.Section;
+                item.SectionId = sectionItem.SectionId;
+                item.SortOrder = sectionItem.SortOrder;
+                item.SubSection = sectionItem.SubSection;
+                item.Title = sectionItem.Title;
+                item.ContentText = sectionItem.ContentText;
+                item.ContentUrl = sectionItem.ContentUrl;
+                item.ContentUrlText = sectionItem.ContentUrlText;
+
+                if (insert)
+                {
+                    repo.SectionContents.Add(item);
+                }
+
+                repo.SaveChanges();
+            }
         }
 
     }
