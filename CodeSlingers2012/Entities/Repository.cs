afc5eee15c302fa7bd28e85e41b5ff8017d2eaa3
Ben Baum <benmbaum@gmail.com>
2012-11-17 00:49:40 -0600

    Added view models to map between repository and views

--- CodeSlingers2012/Entities/Repository.cs
+++ CodeSlingers2012/Entities/Repository.cs
@@ -13,9 +13,39 @@ namespace CodeSlingers2012.Entities
 
             using (var repo = new CodeSlingers2012Entities())
             {
-                items = (from r in repo.SectionContents select r).ToList();
+                items = (from r in repo.SectionContents 
+                         orderby r.Section, r.SortOrder
+                         select r).ToList();
             }
             return items;
         }
+
+        public List<SectionContent> GetSectionContentItems(SectionNames section)
+        {
+            var items = new List<SectionContent>();
+
+            using (var repo = new CodeSlingers2012Entities())
+            {
+                items = (from r in repo.SectionContents 
+                         where r.Section == (int)section 
+                         orderby r.SortOrder
+                         select r).ToList();
+            }
+            return items;
+        }
+
+        public SectionContent GetSectionContent(SectionNames section, int sectionId)
+        {
+            var item = new SectionContent();
+
+            using (var repo = new CodeSlingers2012Entities())
+            {
+                item = (from r in repo.SectionContents 
+                        where r.Section == (int)section && r.SectionId == sectionId 
+                        select r).FirstOrDefault();
+            }
+            return item;
+        }
+
     }
 }
