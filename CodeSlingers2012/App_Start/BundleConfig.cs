441f4dc7a23515110ef5bfb0d7fd9155e93a86a3
Patrick Barron <patbarron1@gmail.com>
2012-11-17 02:17:14 -0600

    setup new controller and index page

--- CodeSlingers2012/App_Start/BundleConfig.cs
+++ CodeSlingers2012/App_Start/BundleConfig.cs
@@ -30,6 +30,10 @@ namespace CodeSlingers2012
 
             bundles.Add(new StyleBundle("~/css").Include("~/css/style.css"));
 
+            bundles.Add(new StyleBundle("~/admincss").Include(
+                        "~/css/bootstrap.css", 
+                        "~/css/admin.css"));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
