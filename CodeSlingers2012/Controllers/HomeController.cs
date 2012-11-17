333f1ac9e2f8730aa6f56ea9601b462c7148ab15
Ben Baum <benmbaum@gmail.com>
2012-11-17 00:10:47 -0600

    Added basic database mapping and call

--- CodeSlingers2012/Controllers/HomeController.cs
+++ CodeSlingers2012/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeSlingers2012.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
---
+++ CodeSlingers2012/Entities/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeSlingers2012.Entities
+{
+    public enum SectionNames
+    {
+        Header,
+        HomeImages,
+        HomeLinks,
+        Menu,
+        Location,
+        Jobs,
+        Footer
+    }
+
+    public enum SubSectionNames
+    {
+        Appetizers,
+        Sake,
+        Sushi,
+        Drinks,
+        Soups_Salads,
+        Entrees,
+        HappyHour
+    }
+}
